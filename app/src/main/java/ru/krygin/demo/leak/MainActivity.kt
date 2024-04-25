37c0f89db70a50cd07d0def3016f17f9287bcdec
krygin <krygin.ia@gmail.com>
2024-04-26 02:21:07 +0300

    add Readme.md

--- app/src/main/java/ru/krygin/demo/leak/MainActivity.kt
+++ app/src/main/java/ru/krygin/demo/leak/MainActivity.kt
@@ -1,5 +1,34 @@
 package ru.krygin.demo.leak
 
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity: AppCompatActivity(R.layout.activity_main)
+class MainActivity: AppCompatActivity() {
+
+    private lateinit var webViewContainer: FrameLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WebView.setWebContentsDebuggingEnabled(true)
+        setContentView(R.layout.activity_main)
+        webViewContainer = findViewById(R.id.container)
+        webViewContainer.addView(WebView(this).apply {
+            settings.apply {
+                javaScriptEnabled = true
+                webChromeClient = WebChromeClient()
+            }
+            loadUrl("about:blank")
+        })
+
+    }
+
+    override fun onDestroy() {
+        val webView = webViewContainer.getChildAt(0) as WebView
+        webView.destroy()
+        super.onDestroy()
+
+    }
+}
